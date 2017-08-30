35464ac829a0569cd614e5e6044672ba7c113119
Maximiliano Gabriel Monge <maxi@sneekdigital.co.uk>
2017-08-30 13:29:21 +0100

    General Fixes

---
+++ libs/require-extension-hooks-babel.js
@@ -0,0 +1,31 @@
+const babel = require('babel-core');
+const defaultConfig = {
+    exclude : [/node_modules\/(?!js-unit)/],
+    presets : [
+        ['env', {
+            targets : {
+                node : 'current'
+            }
+        }]
+    ],
+    sourceMaps : true
+};
+let globalConfig = Object.assign({}, defaultConfig);
+
+module.exports = function({content, filename}){
+    for (let x = 0, l = globalConfig.exclude.length; x < l; x++){
+        if (filename.match(globalConfig.exclude[x])){
+            return;
+        }
+    }
+
+    let config = Object.assign({ filename, sourceFileName : filename }, globalConfig);
+    delete config.exclude;
+
+    let result = babel.transform(content, config);
+
+    return { content : result.code, sourceMap : result.map };
+};
+module.exports.configure = function (options) {
+    globalConfig = Object.assign({}, defaultConfig, options);
+};
